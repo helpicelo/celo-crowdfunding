c9f438bb0c5c446157c23db92b24f3f72106eded
Alvaro Lamadrid <aglamadrid19@gmail.com>
2021-03-17 02:54:52 -0400

    add paper overlay

--- kickstart-frontend/src/components/StartPage.js
+++ kickstart-frontend/src/components/StartPage.js
@@ -58,7 +58,17 @@ function StartPage(props) {
           </GridContainer>
         </div>
       </Parallax>
-
+      
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          
+          <div className={"panel-text"}>
+            <p>Do you want to create a Videogame but the lack of resources desincentize you? Do you need liquidity to fund gaming ideas? This is the place for you.</p>
+            <p>In DeBactria you can create campaigns seeking funds, We are the first project in BSC to offer crowdfunding of videogames and gaming projects. DeBactria allows people to solicit funds for an idea, charity, or start-up business related with the gamer community.</p>
+          </div>
+          <Share />
+        </div>
+      </div>
 
       {/* OLD VIDEO INTRO HERO */}
       {/* <div style={{ paddingTop: 20}}>
@@ -72,11 +82,7 @@ function StartPage(props) {
         </div>
       </div> */}
 
-      <div className="panel-text">
-        <p>Do you want to create a Videogame but the lack of resources desincentize you? Do you need liquidity to fund gaming ideas? This is the place for you.</p>
-        <p>In DeBactria you can create campaigns seeking funds, We are the first project in BSC to offer crowdfunding of videogames and gaming projects. DeBactria allows people to solicit funds for an idea, charity, or start-up business related with the gamer community.</p>
-      </div>
-      <Share />
+      
     </div>
 
   );
